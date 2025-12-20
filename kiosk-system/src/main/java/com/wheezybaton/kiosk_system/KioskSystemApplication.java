3e35bd27b867e1fae2cf2a16b7411f02c3b16b91
Sebastian Blaszczyk <s.blaszczyk.633@studms.ug.edu.pl>
2025-12-20 21:39:53 +0100

    Initialize project

---
+++ kiosk-system/src/main/java/com/wheezybaton/kiosk_system/KioskSystemApplication.java
@@ -0,0 +1,13 @@
+package com.wheezybaton.kiosk_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KioskSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KioskSystemApplication.class, args);
+	}
+
+}
