5bc1df3476f83246804387c0e20e1a8bf7b7b4b0
Sebastian Blaszczyk <s.blaszczyk.633@studms.ug.edu.pl>
2025-12-27 21:10:47 +0100

    Add architecture tests (ArchUnit)

---
+++ kiosk-system/src/test/java/com/wheezybaton/kiosk_system/architecture/ArchitectureTest.java
@@ -0,0 +1,44 @@
+package com.wheezybaton.kiosk_system.architecture;
+
+import com.tngtech.archunit.core.importer.ImportOption;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+import com.tngtech.archunit.library.Architectures;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+
+@AnalyzeClasses(packages = "com.wheezybaton.kiosk_system", importOptions = ImportOption.DoNotIncludeTests.class)
+public class ArchitectureTest {
+
+    @ArchTest
+    static final ArchRule services_should_be_in_service_package =
+            classes()
+                    .that().haveSimpleNameEndingWith("Service")
+                    .should().resideInAPackage("..service..");
+
+    @ArchTest
+    static final ArchRule controllers_should_be_in_controller_package =
+            classes()
+                    .that().haveSimpleNameEndingWith("Controller")
+                    .should().resideInAPackage("..controller..");
+
+    @ArchTest
+    static final ArchRule repositories_should_be_in_repository_package =
+            classes()
+                    .that().haveSimpleNameEndingWith("Repository")
+                    .should().resideInAPackage("..repository..");
+
+    @ArchTest
+    static final ArchRule layered_architecture_must_be_respected =
+            Architectures.layeredArchitecture()
+                    .consideringOnlyDependenciesInAnyPackage("com.wheezybaton.kiosk_system..")
+                    .layer("Controller").definedBy("..controller..")
+                    .layer("Service").definedBy("..service..")
+                    .layer("Repository").definedBy("..repository..")
+                    .layer("Config").definedBy("..config..") // Dodajemy warstwę Config
+
+                    .whereLayer("Controller").mayNotBeAccessedByAnyLayer()
+                    .whereLayer("Service").mayOnlyBeAccessedByLayers("Controller", "Service", "Config")
+                    .whereLayer("Repository").mayOnlyBeAccessedByLayers("Service", "Config");
+}
